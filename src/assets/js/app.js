4394b4d34a256dec8a42d6101a496b686e7564a0
kgroza <kgrozanski@gmail.com>
2019-05-18 23:01:38 +0200

    v.1.0.5 + buggy tile selection added

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,8 +1,12 @@
 // import 'bootstrap';
 import '../scss/style.scss';
-//import * as $ from 'jquery';
-//import { fromEvent } from 'rxjs';
-//import { throttleTime } from 'rxjs/operators';
+import * as $ from 'jquery';
+import {
+  fromEvent
+} from 'rxjs';
+import {
+  throttleTime
+} from 'rxjs/operators';
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
@@ -14,16 +18,17 @@ class Map {
   constructor(width, height, tileSize) {
     this.width = width;
     this.height = height;
-    this.tileSize = tileSize / 2 || 25;
+    this.tileHeight = tileSize / 2 || 25;
+    this.tileWidth = Math.round(Math.tan(1.04719755) * this.tileHeight);
     this.mapArray = [];
   }
 
   generateMap() {
     let id = 0,
-      rowOffset = -this.tileSize,
+      rowOffset = -this.tileHeight,
       tileDimensions = {
-        height: this.tileSize,
-        width: Math.round(Math.tan(1.04719755) * this.tileSize)
+        height: this.tileHeight,
+        width: this.tileWidth
       },
       posCalc = {
         x: 0,
@@ -56,6 +61,84 @@ class Map {
       });
     });
     console.log(this.mapArray);
+
+    //Execute observable for mouse inputs
+    this.addListener();
+  }
+
+  addListener() {
+    fromEvent(canvas, 'click').pipe(throttleTime(50)).subscribe((e) => {
+      this.selectTile(e.clientX, e.clientY);
+    });
+  }
+
+  selectTile(clientX, clientY) {
+    let fullRows = Math.ceil((clientY / this.tileHeight)),
+      fullCols,
+      pointInTileOffsetX,
+      pointInTileOffsetY,
+      checkingHeight,
+      direction = 0;
+
+    if (fullRows % 2) {
+      fullCols = Math.abs(Math.floor(clientX / (this.tileWidth * 2)));
+      pointInTileOffsetX = clientX - (fullCols * (this.tileWidth * 2));
+    } else {
+      fullCols = Math.abs(Math.ceil((clientX - this.tileWidth) / (this.tileWidth * 2)));
+      pointInTileOffsetX = (fullCols * (this.tileWidth  * 2)) - clientX;
+    };
+
+    // if (clientY >= ((this.tileHeight * fullRows) + this.tileHeight)) {
+    //   fullRows += 2;
+    // }
+
+    let selectedRhombus = this.mapArray[fullRows][fullCols];
+
+
+    //Calculate coordinates inside tile area to find 
+    //if mouse covers currently selected tile
+    //depending
+    
+    if (pointInTileOffsetX > this.tileWidth ) {
+      direction = 1;
+      pointInTileOffsetX = clientX - ((fullCols + 1) * (this.tileWidth * 2));
+    }
+
+    pointInTileOffsetY = Math.ceil((Math.tan(0.523598776) * Math.abs(pointInTileOffsetX)));
+    checkingHeight = (this.tileHeight * fullRows) - clientY;
+    console.log(clientX - (fullCols * (this.tileWidth)))
+
+    if (checkingHeight > pointInTileOffsetY) {
+      let grabRow = fullRows - 1;
+    
+      if (pointInTileOffsetX < this.tileWidth && direction == 0) {
+        selectedRhombus = this.mapArray[grabRow][fullCols];
+      } else {
+        let grabCol = fullCols + 1;
+        selectedRhombus = this.mapArray[grabRow][grabCol];
+      }
+
+    }
+
+
+
+    let logging = {
+      'fullRows': fullRows,
+      'clientY': clientY,
+      'fullCols': fullCols,
+      'clientX': clientX,
+      'pointInTileOffsetX': pointInTileOffsetX
+    }
+    console.log(logging);
+    console.log(this.mapArray[fullRows][fullCols]);
+
+
+
+
+
+    //Rerender selected tile
+    ctx.fillStyle = "#00FF00";
+    ctx.fill(new Path2D(`M${selectedRhombus.x} ${selectedRhombus.y} l ${this.tileWidth} ${this.tileHeight} l -${this.tileWidth} ${this.tileHeight} l -${this.tileWidth} -${this.tileHeight}`));
   }
 
 }
@@ -102,9 +185,3 @@ map.generateMap();
 //     `);
 //   });
 // }
-
-//fromEvent(document, 'click').pipe(throttleTime(50)).subscribe((e) => {
-//  $('body').append(`
-//    <div class="pointer" style="left: ${e.clientX}px; top: ${e.clientY}px"></div>
-//  `);
-//});
